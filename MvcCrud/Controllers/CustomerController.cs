e774424d82ec6ffaacd7a1a11984a4a95794ffe6
Michael Kenechukwu <michaelkingsleykc@gmail.com>
2023-11-25 04:29:10 -0800

    Add project files.

---
+++ MvcCrud/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace MvcCrud.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit(string code)
+        {
+            ViewData["editcode"] = code;
+            return View(); 
+        }
+    }
+}
